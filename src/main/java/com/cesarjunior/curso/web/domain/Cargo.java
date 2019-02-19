7fb4c56ad622d97e30df102d332d2f55599e6b5b
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-02-18 21:56:23 -0300

    Inclusao domain e mapeamento ORM - Departamento e Cargo

---
+++ src/main/java/com/cesarjunior/curso/web/domain/Cargo.java
@@ -0,0 +1,35 @@
+package com.cesarjunior.curso.web.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@SuppressWarnings("serial")
+@Entity
+public class Cargo extends AbstractEntity<Long> {
+
+	@Column(nullable = false, unique = true, length = 60)
+	private String nome;
+
+	@ManyToOne
+	@JoinColumn(name = "id_departamento_fk")
+	private Departamento departamento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+}
